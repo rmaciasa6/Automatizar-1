2cdc4d9908cdb9e249e59a184f6a28c627df1dc5
rmaciasa6 <rmaciasa6@unemi.edu.ec>
2025-09-27 16:21:40 -0500

    Cuarto Envio

--- Tests/test_Calculation.py
+++ Tests/test_Calculation.py
@@ -8,5 +8,8 @@ class TestCalculator(unittest.TestCase):
     def test_resta(self):
         self.assertEqual(restar(5, 3), 2)
 
+    def test_multipicacion(self):
+        self.assertEqual(multiplicar(5, 3), 15)
+
 if __name__ == '__main__':
     unittest.main()
--- src/calculator.py
+++ src/calculator.py
@@ -2,5 +2,7 @@ def suma(a,b):
     return a + b
 def restar(a,b):
     return a - b
+def multiplicar(a,b):
+    return a * b
 
 print(restar(5,3))
