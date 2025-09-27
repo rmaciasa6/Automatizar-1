e1a4e66da377f1e26659cd68fe005d593342c7bb
rmaciasa6 <rmaciasa6@unemi.edu.ec>
2025-09-27 13:50:26 -0500

    PRUEBA CON CAMBIO

--- Tests/test_Calculation.py
+++ Tests/test_Calculation.py
@@ -3,7 +3,7 @@ from src.calculator import *
 
 class TestCalculator(unittest.TestCase):
     def test_suma(self):
-        self.assertEqual(suma(2, 3), 15)
+        self.assertEqual(suma(2, 3), 5)
 
     def test_resta(self):
         self.assertEqual(restar(5, 3), 2)
